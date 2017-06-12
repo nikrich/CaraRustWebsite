b92d2fa1cf18ab39242343a3c5d9f78fbb77ad00
Jannik Richter <jannik811@gmail.com>
2017-06-12 17:49:45 +0200

    New

---
+++ app/scripts/main.js
@@ -0,0 +1,26 @@
+/*
+ *  Menu navigation
+ */
+
+$("#explore,#btnAbout").click(function () {
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
+    }, 500);
+});
+
+$("#btnWork").click(function () {
+    $('html, body').animate({
+        scrollTop: $("#work").offset().top
+    }, 500);
+});
+
+$("#btnContact").click(function () {
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top
+    }, 500);
+});
+
+//$('iframe').load(function (handler) {
+//    console.log(handler);
+//    $(handler.target).contents().find('.ytp-thumbnail-overlay-image').addClass('crop');
+//});
